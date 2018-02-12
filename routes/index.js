5cc418756ebea258f8d9ed3768e0cfbec2655b4f
SHENGKE XU <xushengke94@gmail.com>
2018-02-12 12:33:59 -0500

    ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const templateTypes = [
+  { name: "Jade", usefulness: "sucky"},
+  { name: "EJS", usefulness: "a little better"},
+  { name: "Handlebars", usefulness: "awesome"}
+]
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' , message: "i hate this"});
+  res.render('index', {
+  title: 'Express' ,
+  message: "i hate this",
+  templates : templateTypes
+  });
 });
 
 module.exports = router;
